c45f838045faba28d216e6b64f8aa6240acce5b2
Melvin Arellano <lmelvin.arellano@gmail.com>
2023-04-26 13:06:27 +0800

    tooltips

--- frontend/src/pages/Local/LocalDashboard.jsx
+++ frontend/src/pages/Local/LocalDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { BsFillGearFill, BsBox, BsBoxSeam } from 'react-icons/bs'
+import { BsFillGearFill, BsBox, BsBoxSeam, BsTrashFill, BsStarFill, BsStar, BsThreeDots, BsCheckLg } from 'react-icons/bs'
 import { GoPlus } from 'react-icons/go'
+import { RxCross2 } from 'react-icons/rx'
 import { useTodos } from '../../data/context/TodosContext'
 import AddTaskModal from './AddTaskModal'
 
@@ -8,6 +9,7 @@ const LocalDashboard = () => {
 	const {tasks, setTasks, colums, setColumns} = useTodos()
 	const [addColumnToggle, setAddColumnToggle] = useState(true)
 	const [addColumn, setAddColumn] = useState('')
+	const [tooltip, setTooltip] = useState('')
 	const [destinationColumn, setDestinationColumn] = useState('')
 
 	const handleAddColumn = e => {
@@ -32,6 +34,28 @@ const LocalDashboard = () => {
 		}
 	}
 
+	const removeMark = id => {
+		const localStored = JSON.parse(localStorage.getItem('p1project'))
+		const indexOfTarget = localStored.tasks.findIndex(item => item.id === id)
+
+		localStored.tasks[indexOfTarget].marks.marked = !localStored.tasks[indexOfTarget].marks.marked
+
+		localStorage.setItem('p1project', JSON.stringify(localStored))
+		
+		fetchLocalstorage()
+	}
+
+	const deleteTask = id => {
+		const localStored = JSON.parse(localStorage.getItem('p1project'))
+		const indexOfTarget = localStored.tasks.findIndex(item => item.id === id)
+
+		localStored.tasks[indexOfTarget].marks.deleted = true
+
+		localStorage.setItem('p1project', JSON.stringify(localStored))
+		
+		fetchLocalstorage()
+	}
+
 	useEffect(() => {
 		fetchLocalstorage()
 	}, [destinationColumn])
@@ -52,7 +76,7 @@ const LocalDashboard = () => {
 							addColumnToggle && (
 								<div className='flex items-center justify-between gap-2 h-[38px] mt-7'>
 									<input type="text" name='column' className='h-full px-3 border shadow-sm w-full block rounded' value={addColumn} onChange={handleAddColumn} placeholder='Column name...' />
-									<button className='bg-[#4ECCA3] h-full px-4 gap-1 rounded shadow-sm text-white flex items-center font-medium hover:brightness-95 duration-200' onClick={handleAddColumnSubmit}>
+									<button className='bg-[#4ECCA3] h-full px-4 gap-1 rounded shadow-sm text-white flex items-center font-medium hover:brightness-95 duration-200' onClick={handleAddColumnSubmit} disabled={!addColumn}>
 										<GoPlus />
 										<span>Add</span>
 									</button>
@@ -69,15 +93,31 @@ const LocalDashboard = () => {
 										</div>
 										<div className='flex flex-col gap-2 overflow-y-auto'>
 											{
-												tasks.tasks.filter(todo => todo.column === task).length > 0 ? tasks.tasks.filter(todo => todo.column === task).map(item => (
-													<div key={item?.id} className='flex items-center gap-2 h-[28px] px-2 hover:bg-[#f8f8f8] duration-300 rounded-sm cursor-pointer relative task-item'>
-														<span><BsBox /></span>
-														<p className='truncate'>{item?.title}</p>
-														<div className='h-full bg-orange-300 hidden duration-200 items-center gap-1 absolute right-0 task-item-action'>
-															<button className='w-[20px] aspect-square bg-blue-300'>M</button>
-															<button className='w-[20px] aspect-square bg-blue-300'>D</button>
+												tasks.tasks.filter(todo => todo.column === task).length > 0 ? tasks.tasks.filter(todo => todo.column === task && !todo.marks.deleted).map(item => (
+														<div key={item?.id} onMouseLeave={() => setTooltip('')} className='flex items-center gap-2 h-[28px] px-2 hover:bg-[#f8f8f8] duration-300 rounded-sm relative task-item'>
+															<span><BsBox /></span>
+															<p className={(tooltip === item?.id ? 'w-[54%]' : 'task-item-text w-[84%]') + ' truncate '}>{item?.title}</p>
+															<div className='h-full hidden duration-200 items-center gap-1 absolute right-2 task-item-action	z-20'>
+																{
+																	tooltip === item?.id && (
+																		<div className='flex items-center gap-1 bg-[#f8f8f8]'>
+																			<button onClick={() => deleteTask(item?.id)} className='w-[22px] hover:text-white hover:bg-[#393E46] aspect-square rounded-md grid place-items-center duration-200'><BsTrashFill /></button>
+																			<button onClick={() => setTooltip(item?.id)} className='w-[22px] hover:text-white hover:bg-[#393E46] aspect-square rounded-md grid place-items-center duration-200'><BsCheckLg /></button>
+																			<button onClick={() => removeMark(item?.id)}  className='w-[22px] hover:text-white hover:bg-[#393E46] aspect-square rounded-md grid place-items-center duration-200'>{ item?.marks.marked ? <span className='text-sunglow'><BsStarFill /></span> : <span><BsStar /></span> }</button>
+																		</div>
+																	)
+																}
+																<button onClick={() => item?.id === tooltip ? setTooltip('') : setTooltip(item?.id)} className='w-[22px] hover:text-white hover:bg-[#393E46] aspect-square rounded-md grid place-items-center duration-200'>{ item?.id === tooltip ? <RxCross2 /> : <BsThreeDots /> }</button>
+															</div>
+															{
+																tooltip !== item?.id && (
+																	<div className='absolute right-2 task-mark'>
+																		<span className='w-[22px] aspect-square rounded-md grid place-items-center duration-200'>{ item?.marks.marked && <span className='text-sunglow'><BsStarFill /></span> }</span>
+																	</div>
+																)
+															}
+
 														</div>
-													</div>
 												)) : (
 													<div className='flex items-center gap-2 h-[28px] px-2 hover:bg-[#f8f8f8] duration-300 rounded-sm cursor-pointer'>
 														<span><BsBoxSeam /></span>
