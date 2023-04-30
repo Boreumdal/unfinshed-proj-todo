1b8cd55f218c691b4700ae9b24013a27c4ac14b3
Melvin Arellano <lmelvin.arellano@gmail.com>
2023-04-30 20:21:15 +0800

    search functionality done

--- frontend/src/pages/Local/Search.jsx
+++ frontend/src/pages/Local/Search.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsSearch, BsFolder2, BsCalendar2Heart } from 'react-icons/bs'
 import { MdClose } from 'react-icons/md'
+import { useTodos } from '../../data/context/TodosContext'
 
 const Search = ({ setSearchToggle }) => {
-	useEffect(() => {
-		let a = 0
-		const aaa = setInterval(() => {
-			a++
-			console.log('Executed = '+ a)
-			clearer()
-		}, 1000)
-		function clearer(){
-			if (a === 3){
-				clearInterval(aaa)
+	const { tasks } = useTodos()
+	const [searchTitle, setSearchTitle] = useState('')
+	const [result, setResult] = useState([])
 
-			}
+	useEffect(() => {
+		if (searchTitle){
+			let filtered = tasks.tasks.filter(task => task.title.toLowerCase().includes(searchTitle.toLowerCase()))
+
+			setResult(filtered)
+
 		}
-	}, [])
+	}, [searchTitle])
+	
 	return (
 		<div className='absolute grid place-items-center inset-0 bg-[#3d3d3d2e]'>
 			<div className='w-[548px] bg-white rounded-lg overflow-hidden'>
 				<div className='h-[44px] w-full flex items-center border gap-1'>
 					<span className='text-xl h-full aspect-square grid place-items-center'><BsSearch /></span>
-					<input type="text" className='bg-transparent w-full h-full outline-none border-none font-medium' placeholder='Search by title...' />
+					<input type="text" className='bg-transparent w-full h-full outline-none border-none font-medium' value={searchTitle} onChange={e => setSearchTitle(e.target.value)} placeholder='Search by title...' />
 					<span className='text-2xl h-full aspect-square grid place-items-center'><MdClose /></span>
 				</div>
 				<div className='flex flex-col'>
@@ -30,60 +30,20 @@ const Search = ({ setSearchToggle }) => {
 						<h1 className='text-lg font-medium'>Results</h1>
 					</div>
 					<div className='grid grid-cols-2 gap-1 px-2 pb-2'>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
-						<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
-							<div className='flex items-center gap-2'>
-								{
-									<span><BsCalendar2Heart /></span>
-								}
-								<h1 className='font-medium truncate max-w-[140px]'>Lorem ipsum dolor sit amet.</h1>
-							</div>
-							<span className='text-lg'><BsFolder2 /></span>
-						</div>
+
+						{
+							result?.map(task => (
+								<div className={' bg-[#EEEEEE] hover:brightness-105 cursor-pointer hover:shadow-none duration-200 font-medium w-full h-[35px] flex items-center justify-between px-3 rounded shadow-sm'}>
+									<div className='flex items-center gap-2'>
+										{
+											<span><BsCalendar2Heart /></span>
+										}
+										<h1 className='font-medium truncate max-w-[140px]'>{task.title}</h1>
+									</div>
+									<span className='text-lg'><BsFolder2 /></span>
+								</div>
+							))
+						}
 					</div>
 
 				</div>
